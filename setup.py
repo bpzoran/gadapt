e8767dc000cd2127111175bf5da001ab6023aea9
Zoran Jankovic <jankovicz@gmail.com>
2023-05-26 00:06:02 +0200

    README.md updated Version increase.

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name='gadapt',
-    version='0.2.12',
+    version='0.2.13',
     author="Zoran Jankovic",
     author_email='bpzoran@yahoo.com',
     url='https://github.com/bpzoran/gadapt',
     packages=setuptools.find_packages(),
-    long_description=long_description,
+    long_description="GAdapt: Self-Adaptive Genetic Algorithm",
     description="GAdapt: A Python Library for Self-Adaptive Genetic Algorithm."
 )
