625fc3196a2e02e61d6dc0c80a0778a78bc02fc3
Zoran Jankovic <jankovicz@gmail.com>
2024-05-19 11:03:48 +0200

    version

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="gadapt",
-    version="0.3.6",
+    version="0.4.0",
     author="Zoran Jankovic",
     author_email="bpzoran@yahoo.com",
     url="https://github.com/bpzoran/gadapt",
     packages=find_packages(),
     long_description=open("README.md").read(),
-    # Specify the content type explicitly
     long_description_content_type="text/markdown",
     description="GAdapt: A Python Library for\
         Self-Adaptive Genetic Algorithm.",
