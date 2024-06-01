63464a31d9e88f600e6732793b423edd2a84f395
Zoran Jankovic <jankovicz@gmail.com>
2024-06-01 13:23:03 +0200

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gadapt",
-    version="0.4.15",
+    version="0.4.16",
     author="Zoran Jankovic",
     author_email="bpzoran@yahoo.com",
     url="https://github.com/bpzoran/gadapt",
